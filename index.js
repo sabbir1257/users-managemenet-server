071b1f550c756e2db4c0bd9f746081a62066b0b6
sabbir1257 <masabbir1257@gmail.com>
2025-02-26 09:07:51 +0600

    Send Client data to the server and display data on the client

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
+
 const users = [
   { id: 1, name: "Sabana", email: "sabana@gmail.com" },
   { id: 2, name: "Sabnoor", email: "sabnoor@gmail.com" },
@@ -23,6 +25,11 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     console.log('post api heading')
     console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+
 })
 
 app.listen(port, () => {
